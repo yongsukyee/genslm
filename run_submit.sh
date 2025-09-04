cc0d58b99bdb4f2f2829360d5f30e6f28a114c9a
Suk Yee Yong <Sukyee.Yong@csiro.au>
2025-09-05 09:14:07 +1000

    update run script template

--- run_submit.sh
+++ run_submit.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 ENV_PATH="/scratch3/${USER}/genslm_wgsi/genslm_wgsi.env"
-
-# Using venv
-module load python
-module load pytorch
-module load transformers/4.34.1-py312
 source ${ENV_PATH}/bin/activate
 
 python -m genslm.hpc.submit -T virga -A <ACCOUNT> \
